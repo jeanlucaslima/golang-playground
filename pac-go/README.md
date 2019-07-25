9d1cb2306af79a9766afdceeaf29245605348b8a
Jean Lucas Lima <jeanleonino@gmail.com>
2019-07-25 02:06:38 -0300

    Pac-Go exercise #1

---
+++ pac-go/README.md
@@ -0,0 +1,5 @@
+# pac-go
+
+A Pac Man clone writtenin Go (with emojis!)
+
+This project is based on https://github.com/danicat/pacgo
---
+++ pac-go/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func loadMaze() error {
+	f, err := os.Open("maze01.txt")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		maze = append(maze, line)
+	}
+
+	return nil
+}
+
+var maze []string
+
+func printScreen() {
+	for _, line := range maze {
+		fmt.Println(line)
+	}
+}
+
+func main() {
+	// lading resources
+	err := loadMaze()
+	if err != nil {
+		log.Printf("Error loading maze: %v\n", err)
+		return
+	}
+
+	// game loop
+	for {
+		// update screen
+		printScreen()
+
+		break
+	}
+}
